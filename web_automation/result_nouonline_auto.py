1651efdd1725be7c6c384d7f97665aa2a5c0ed98
akenz1901 <michael_aka1@icloud.com>
2024-02-26 14:40:29 +0100

    Updated executable location

--- web_automation/result_nouonline_auto.py
+++ web_automation/result_nouonline_auto.py
@@ -12,7 +12,7 @@ options.add_argument("--no-sandbox")
 options.add_argument('--profile-directory=Profile 2')
 options.add_argument('----headless=new')
 
-driver = webdriver.Chrome(options=options, executable_path=r"C:\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(options=options, executable_path=r"C:/chromedriver_win32/chromedriver.exe")
 stealth(driver,
         user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
         languages=["en-US", "en"],
@@ -27,6 +27,8 @@ driver.get("https://www.nouonline.net")
 
 # # dropdowns = driver.find_elements(by=By.CSS_SELECTOR, value="dropdown-menu")
 # # for element in dropdowns:
+
+
 # #     if element.text == "Students":
 # #         element.click()
 #
